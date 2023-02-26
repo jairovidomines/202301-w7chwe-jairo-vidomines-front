60ecab3d85607cb77280c5539cb0828f72b7fe6f
Jairo Vidomines <jvidomines@gmail.com>
2023-02-26 19:43:27 +0100

    Add styles to Layout component

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,13 +1,14 @@
 import { Outlet } from "react-router-dom";
 import LoginForm from "../LoginForm/LoginForm";
+import LayoutStyled from "./LayoutStyled";
 
 const Layout = (): JSX.Element => {
   return (
-    <>
+    <LayoutStyled>
       <h1>Social App</h1>
       <Outlet />
       <LoginForm />
-    </>
+    </LayoutStyled>
   );
 };
 
---
+++ src/components/Layout/LayoutStyled.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const LayoutStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  h1 {
+    padding-bottom: 15px;
+  }
+`;
+
+export default LayoutStyled;
