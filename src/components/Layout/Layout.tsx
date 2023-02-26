6009203c9359109053dcec0b8ed0c3cf8cfb5c7c
Jairo Vidomines <jvidomines@gmail.com>
2023-02-26 20:14:15 +0100

    Create LoginPage component

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,5 +1,4 @@
 import { Outlet } from "react-router-dom";
-import LoginForm from "../LoginForm/LoginForm";
 import LayoutStyled from "./LayoutStyled";
 
 const Layout = (): JSX.Element => {
@@ -7,7 +6,6 @@ const Layout = (): JSX.Element => {
     <LayoutStyled>
       <h1>Social App</h1>
       <Outlet />
-      <LoginForm />
     </LayoutStyled>
   );
 };
