7e520c2bd93d036925e721454a85b86905f9f6c6
Jairo Vidomines <jvidomines@gmail.com>
2023-02-26 19:28:57 +0100

    Create routes

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
+import { RouterProvider } from "react-router-dom";
 import { store } from "./store/store";
 import reportWebVitals from "./reportWebVitals";
-import App from "./components/App/App";
 import GlobalStyles from "./styles/GlobalStyles";
+import { router } from "./routes/routes";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
@@ -13,7 +14,7 @@ root.render(
   <React.StrictMode>
     <GlobalStyles />
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
 );
